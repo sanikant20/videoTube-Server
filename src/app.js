fd6866712724ec97b4c26db90d03806c6608fb96
Sanikant Kushwaha <141299594+sanikant20@users.noreply.github.com>
2024-09-05 13:52:33 +0545

    added cors and cookieparser configurstion

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+// cross origin resource sharing configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// Main configuration
+app.use(express.json({ limit: "16kb" })) // It's limit the json file size
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // URL encoded extended i.e., sanikant+kushwaha+423ffoijf, sanikant%kushwaha%lksjf893
+app.use(express.static("public")) // public assets store files like images, etc.
+app.use(cookieParser())
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+// import { SERVER_PORT } from "./constant.js";
 
 dotenv.config({
     path: '/.env'
 })
 
+// Database connection is called here
 connectDB()
+    .then(() => {
+
+        //Throw error while there is an error
+        app.on("error", (error) => {
+            console.log("Error : ", error)
+            throw error
+        })
+
+        // Listen port for server
+        app.listen(`${process.env.PORT || 8000}`, () => {
+            console.log(`Server is running at PORT : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`MongoDB connection failed !!!`, err)
+    })
